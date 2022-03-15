b1df3adaa7a9fba2a1ff621a532bc5eb3b4da244
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-03-14 21:36:26 -0500

    added moment to get current day

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,10 @@
 //need moment js to get current date and time
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+
+var currentTime = moment ();
+
+currentTime = currentTime.startOf("hour");
+
+
+//contribution:https://demo.mobiscroll.com/jquery/agenda/load-events-on-demand#
 
